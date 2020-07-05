24d34cc5873a87784a0b8c4bb26672cec55045b2
hppRC <hpp.ricecake@gmail.com>
2020-07-06 01:26:55 +0900

    :up: Update dependencies

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /* eslint-disable no-param-reassign */
 const sitemap = require(`nextjs-sitemap-generator`);
+const withOffline = require(`next-offline`);
+const withOptimizedImages = require(`next-optimized-images`);
 
 sitemap({
-  baseUrl: process.env.NEXT_PUBLIC_GITHUB_PAGES ? `hpprc.github.io` : `next-tempalte.hpprc.com`,
+  baseUrl: process.env.NEXT_PUBLIC_GITHUB_PAGES ? `hpprc.dev` : `next-tempalte-tailwind.hpprc.com`,
   pagesDirectory: `${__dirname}/src/pages`,
   targetDirectory: `public/`,
 });
 
-module.exports = {
-  target: `serverless`,
-  assetPrefix: process.env.NEXT_PUBLIC_GITHUB_PAGES ? `/next-template` : ``,
-};
+module.exports = withOffline(
+  withOptimizedImages({
+    // target: `serverless`,
+  })
+);
