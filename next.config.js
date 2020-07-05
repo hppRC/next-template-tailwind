da8bcf91c7c68076e7ce701cfd5c2affefab4b93
hppRC <hpp.ricecake@gmail.com>
2020-07-06 01:46:40 +0900

    🥺chore

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const withOffline = require(`next-offline`);
 const withOptimizedImages = require(`next-optimized-images`);
 
 sitemap({
-  baseUrl: process.env.NEXT_PUBLIC_GITHUB_PAGES
-    ? `hpprc.dev/next-template-tailwind`
-    : `next-tempalte-tailwind.hpprc.com`,
   pagesDirectory: `${__dirname}/src/pages`,
   targetDirectory: `public/`,
 });
